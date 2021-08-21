04962d8f19408b5b88c54d946bb553abb90b6a41
KesavanG <kesavang4020@gmail.com>
2021-08-21 20:36:48 +0530

    violet:Import kernel Configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,3 +47,21 @@ TARGET_BOARD_PLATFORM := sm6150
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
+
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := violet_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/violet
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += service_locator.enable=1
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_BOOTIMG_HEADER_VERSION := 1
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+
