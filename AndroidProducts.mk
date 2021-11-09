81d43764cafdabc5a325238e7ccc45853f40e04f
KesavanG <kesavang4020@gmail.com>
2021-11-09 17:56:24 +0530

    spark os bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_violet.mk
+    $(LOCAL_DIR)/spark_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_violet-user \
-    lineage_violet-userdebug \
-    lineage_violet-eng
+    spark_violet-user \
+    spark_violet-userdebug \
+    spark_violet-eng
--- spark_violet.mk
+++ spark_violet.mk
@@ -22,9 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_violet
+PRODUCT_NAME := spark_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
